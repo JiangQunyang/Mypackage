f037555aac24c43e1de7a0d768e60b25e4602114
JiangQunyang <qj566@york.ac.uk>
2016-11-25 14:35:31 +0000

    This is a whole script

--- plot NADPH concentration.R
+++ plot NADPH concentration.R
@@ -1,8 +1,11 @@
-#This is the standar NADPH curve,ploting the line graph between abs(absorbance)~concentration(ctr).
+#This is the standar NADPH curve by spectrophotomete,ploting the line graph between abs(absorbance)~concentration(ctr).
 #This is under 340nm(wavelenth),the extincyion coefficient(molbs) is 6.22(M-1*cm-1), and normally, the lt(lenth of cuvette)=1(cm)
 
 #plot graph
-stdNADPH<-function(abs,molbs=6.22,lt=1){
+library(ggplot2)
+stdNADPH<-function(abs,molbs=6.22,lt=1,line=TRUE){
+
+  if(line==TRUE){
   ctr<-abs/(molbs*lt)
   df<-data.frame(ctr,abs)
   p<-ggplot(data=df,aes(x=ctr,y=abs))+
@@ -10,8 +13,33 @@ stdNADPH<-function(abs,molbs=6.22,lt=1){
     stat_smooth(method="lm",se=FALSE)+
     xlab("NADPH concentration (mol/L)")+
     ylab("Absorbance (340nm)")+
-    theme_bw()
+    theme_bw()+
+    annotate(geom="text", x=max(abs)/7, y= 0.6, label="Abs         Ctr",  color="red")
+
+  for (i in 1:length(abs)){
+    p <- p + annotate(geom="text", x =max(abs)/7.45, y = (0.6 - i/20), label=abs[i])
+    p <- p + annotate(geom="text", x = max(abs)/6.50, y = (0.6 - i/20), label=round(ctr[i], 3))
+  }
+  return(p)
+  }
+  else{
+    ctr<-abs/(molbs*lt)
+    df<-data.frame(ctr,abs)
+    p<-ggplot(data=df,aes(x=ctr,y=abs))+
+      geom_point()+
+      xlab("NADPH concentration (mol/L)")+
+      ylab("Absorbance (340nm)")+
+      theme_bw()+
+      annotate(geom="text", x=max(abs)/7, y= 0.6, label="Abs         Ctr",  color="red")
+
+    for (i in 1:length(abs)){
+      p <- p + annotate(geom="text", x =max(abs)/7.45, y = (0.6 - i/20), label=abs[i])
+      p <- p + annotate(geom="text", x = max(abs)/6.50, y = (0.6 - i/20), label=round(ctr[i], 3))
+    }
     return(p)
+  }
+
 }
 
-#this is a eg. x<-c(0.2,0.5,0.8,1.3)  stdNADPH(x)
+x<-c(0.2,0.5,0.7,1.3)
+stdNADPH(x,line = TRUE)
