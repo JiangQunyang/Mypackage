7a4f5c5bebf08b2de699bab3bc14753ca86bc10d
LiamHallett <LiamHallett@users.noreply.github.com>
2016-11-25 15:23:22 +0000

    Add files via upload

--- Eadie Hofstee.R
+++ Eadie Hofstee.R
@@ -4,18 +4,19 @@ library(ggplot2)
 
 EadieHofstee <- function(enz.data){
   EHmodel <- lm(enz.data[,2] ~ enz.data[,5])
-  fun_EH <- function(x) y = (EHmodel$coefficients[[2]]*x) + EHmodel$coefficients[[1]]
+  Y.intercept <- c(0, EHmodel$coefficients[[1]])
+  X.intercept <- c(-EHmodel$coefficients[[1]]/EHmodel$coefficients[[2]], 0)
   EHplot <-  ggplot(enz.data, aes(x= enz.data[,5], y= enz.data[,2])) + 
     geom_point(size=3, shape=20) +
     #geom_smooth(method=lm, formula = y ~ x, se=FALSE) +
     ggtitle("Eadie Hofstee Plot") +
     xlab(names(enz.data)[5]) +
     ylab(names(enz.data)[2]) +
-    xlim(0, max(enz.data[,5])) +
-    ylim(0, max(enz.data[,2])) +
+    xlim(0, (X.intercept[1]*1.05)) +
+    ylim(0, (Y.intercept[2]*1.05)) +
     theme_bw() +
     theme(plot.title = element_text(hjust=0.5)) +
-    stat_function(fun=fun_EH,color="blue")
+    geom_abline(intercept = EHmodel$coefficients[[1]], slope = EHmodel$coefficients[[2]], color = "blue", size = 1)
   return(EHplot)
   # need to give formula, intercepts and Km and Vmax
 }
