eb18eb833a12c9582510a83349204030671cd970
LiamHallett <LiamHallett@users.noreply.github.com>
2016-11-25 14:44:54 +0000

    Add files via upload

--- Read in data.R
+++ Read in data.R
@@ -4,8 +4,8 @@ V <- c(72.0,60.1,0,11.1,25.4,44.8,54.5,58.2)
 
 ## Read in to a data frame
 ReadIn <- function(S, V){
-  Inv.V <- 1/S # Lineweaver-Burk
-  Inv.S <- 1/V
+  Inv.V <- 1/V # Lineweaver-Burk
+  Inv.S <- 1/S
   V.div.S <- V/S # Eadie-Hofstee
   S.div.V <- S/V # Hanes-Woolf
   enz.data <- data.frame(S, V, Inv.V, Inv.S, V.div.S, S.div.V) # read into df
